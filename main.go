803fc64b9dcc4d2d6b8f43473e16ce92b99263b2
AndrewI26 <andrewi44@icloud.com>
2025-08-16 22:12:32 -0400

    added routing

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"strconv"
+
 	logging "github.com/AndrewI26/whiz/logger"
-	"github.com/AndrewI26/whiz/router"
+	"github.com/AndrewI26/whiz/routing"
 	"github.com/AndrewI26/whiz/server"
 )
+
+func defaultHandler(params map[string]string) *routing.Response {
+	res := routing.Response{200, "Hello", map[string]string{"My-Header": "Hello world"}}
+	res.Headers["Content-Length"] = strconv.Itoa(len(res.Data))
+	return &res
+}
+
+func hello(params map[string]string) *routing.Response {
+	res := routing.Response{200, "Hello world", map[string]string{"My-Header": "Hello world"}}
+	res.Headers["Content-Length"] = strconv.Itoa(len(res.Data))
+	return &res
+}
 
 func main() {
 	logger := logging.NewLogger(
@@ -21,7 +35,10 @@ func main() {
 	}
 	defer logger.Close()
 
-	router := router.NewRouter()
+	router := routing.NewRouter()
+	router.Get("/user/rentals", defaultHandler)
+	router.Get("/user/:user", defaultHandler)
+	router.PrintRoutes(router.Root, 1)
 
 	server := server.NewServer(8000)
 	server.AddRouter(router)
